2bbc9b8edbd32f6101159404d874026c7faf5305
nabiullahbenish <nabiullah_benash@yahoo.com>
2024-10-22 10:33:34 -0700

    Opening Browser!

---
+++ src/test/java/OpeningBrowser.java
@@ -0,0 +1,34 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class OpeningBrowser {
+
+    public static void main(String[] args) {
+
+
+       // ChromeDriver chromeDriver = new ChromeDriver();
+
+       // chromeDriver.get("https://www.facebook.com/");
+
+
+       // ChromeDriver driver = new ChromeDriver();
+
+       //driver.get("https://naraiman.com/");
+
+
+      //  SafariDriver safariDriver = new SafariDriver();
+
+       // safariDriver.get("https://www.selenium.dev");
+
+        ChromeDriver chrome = new ChromeDriver();
+
+        chrome.get("https://naraiman.com/");
+
+
+
+
+    }
+
+
+
+}
